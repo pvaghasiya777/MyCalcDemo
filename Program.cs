5af83358cb5913b44be3a4be07d54e9f5575f4aa
pvaghasiya777 <paragvaghasiya777@gmail.com>
2023-08-12 12:57:52 +0530

    #Add Programm FIle

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MyCalcDemo
+{
+    public static class MyMath
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static bool isLeapYear(int Year)
+        {
+            if (Year % 400 == 0) return true;
+            if (Year % 100 == 0) return false;
+            if (Year % 4 == 0) return true;
+            return false;
+        }
+
+        public static int FindStringLength(string input)
+        {
+            return input?.Length ?? 0;
+        }
+    }
+}
